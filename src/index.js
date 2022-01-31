0af9e24a1328d3eff00733a5107671a6e0bb6260
Mutawally Nawwar <89324014+Myumet@users.noreply.github.com>
2022-01-31 22:31:31 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.js'
-import reportWebVitals from './reportWebVitals';
-import createRoutes from './component/Route.js';
+import Route from './component/Route'
 
 ReactDOM.render(
-  <App/>,
+  <Route/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
